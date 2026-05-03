082dcd338a2302808bd57e68e1d8e3c0db639b93
kyhun1007 <kyhun1007@naver.com>
2026-05-03 13:06:21 +0900

    feat : login시 userStatus 갱신하는 로직 추가

--- src/main/java/com/sprint/mission/discodeit/controller/AuthController.java
+++ src/main/java/com/sprint/mission/discodeit/controller/AuthController.java
@@ -2,8 +2,11 @@ package com.sprint.mission.discodeit.controller;
 
 import com.sprint.mission.discodeit.controller.api.AuthApi;
 import com.sprint.mission.discodeit.dto.LoginRequest;
+import com.sprint.mission.discodeit.dto.userstatus.UserStatusUpdateRequest;
 import com.sprint.mission.discodeit.entity.User;
 import com.sprint.mission.discodeit.service.AuthService;
+import com.sprint.mission.discodeit.service.UserStatusService;
+import java.time.Instant;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -18,6 +21,7 @@ import org.springframework.web.bind.annotation.RestController;
 public class AuthController implements AuthApi {
 
   private final AuthService authService;
+  private final UserStatusService userStatusService;
 
   @PostMapping("login")
   public ResponseEntity<User> login(@RequestBody LoginRequest loginRequest) {
@@ -28,6 +32,8 @@ public class AuthController implements AuthApi {
     } catch (Exception e) {
       throw new RuntimeException("로그인 실패 : " + e.getMessage());
     }
+
+    userStatusService.updateByUserId(user.getId(), new UserStatusUpdateRequest(Instant.now()));
 
     return ResponseEntity
         .status(HttpStatus.OK)
